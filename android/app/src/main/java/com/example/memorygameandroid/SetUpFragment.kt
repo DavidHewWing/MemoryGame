d2acf5c1c0666a4b800b2ef374aeb2094bbc987f
DavidHewWing <32623283+DavidHewWing@users.noreply.github.com>
2019-09-18 21:37:30 -0400

    almost done layout

--- android/app/src/main/java/com/example/memorygameandroid/SetUpFragment.kt
+++ android/app/src/main/java/com/example/memorygameandroid/SetUpFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_set_up.*
 
 class SetUpFragment : Fragment() {
 
@@ -16,6 +17,16 @@ class SetUpFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_set_up, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpToggleGroup()
+    }
+
+    private fun setUpToggleGroup() {
+        toggleGroup.isSingleSelection = true
+        toggleGroup.check(R.id.matching2)
     }
 
     companion object {
